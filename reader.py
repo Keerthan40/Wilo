d332fab9f40914d0ea669a45485b877dbd8ed0ac
PERAM SREE KEERTHAN REDDY <84032592+Keerthan40@users.noreply.github.com>
2023-05-11 16:56:18 +0530

    Update reader.py

--- reader.py
+++ reader.py
@@ -2,52 +2,67 @@
 
 from global_params import *
 import time
-
+import datetime
+from tag import *
 ''' Class of Reader states'''
 class ReaderState(object):
     IDLE = 0
-    SLEEP = 5
+    
+    
+    SLEEP_SAMPLE = 5
+    SAMPLE_DATA = 10
     # SELECT command: variable length
     # 1010 (4) | Target (3) | Action (3) | MemBank (2) | Pointer (EBV) |
     # Length (8) | Mask (variable) | Truncate (1) | CRC (16) |
-    SELECT = 10
-
-    SELECT_RX = 15
 
     # QUERY command: 22 bits
     # 1000 (4) | DR (1) | M (2) | TRext (1) | Sel (2) | Session (2) | Target (1) | Q (4) | CRC (5) |
+    SLEEP_QUERY = 15
     QUERY = 20
-
     # Tag Reply command: 16 bits
     # RN16 (16)
     QUERY_RX = 25
 
     QUERY_REP = 30
     QUERY_REP_RX = 35
-    ACK = 40
-    ACK_RX = 45
-    RN16 = 50 # fix?
-    SAMPLE_DATA = 55
-    SENSOR_DATA = 60 
-    SENSOR_DATA_RX = 65
-    SENSOR_DATA_ACK = 70
-    SENSOR_DATA_ACK_RX = 75
-    Req_RN16 = 80
-    Tag_ID = 85
-
+    QUERY_ACK = 40
+    QUERY_ACK_RX = 45
+    QUERY_RN16 = 50 # fix?
+    QUERY_REQ_RN16 = 55
+    TAG_ID = 60
+    TAG_ID_RX = 65   
+    
+    SLEEP_SENSOR_DATA = 70
+    SELECT = 75
+    SELECT_RX = 80
+    SENSOR_DATA = 85
+    SENSOR_DATA_RX = 90
+    SENSOR_DATA_ACK = 95
+    SENSOR_DATA_ACK_RX = 100
+    SENSOR_DATA_RN16 = 105
+    SENSOR_DATA_REQ_RN16 = 110
+    DATA = 115
+    SAMPLE_DATA_RX = 120
+    SELECT_QUERY = 125
+    SELECT_RX_QUERY = 130
 ''' Class of Reader params'''
 class ReaderParam(object):
 
     # timing
-    timing = {ReaderState.SELECT: 0.01,
+    timing = {ReaderState.SELECT: 1,
               ReaderState.QUERY: QUERY_BITS/BLF,
-              ReaderState.RN16: RN16_BITS/BLF,
-              ReaderState.Req_RN16:RN16_BITS/BLF,
-              ReaderState.QUERY_REP: 0.01,
-              ReaderState.ACK: 0.01,
-              ReaderState.SAMPLE_DATA: 0.01,
-              ReaderState.SENSOR_DATA:0.01,
-              ReaderState.SLEEP:0.00001}
+              ReaderState.QUERY_RN16: RN16_BITS/BLF,
+              ReaderState.SENSOR_DATA_RN16: RN16_BITS/BLF,
+              ReaderState.QUERY_REQ_RN16:RN16_BITS/BLF,
+              ReaderState.SENSOR_DATA_REQ_RN16:RN16_BITS/BLF,
+              ReaderState.QUERY_REP: 1,
+              ReaderState.QUERY_ACK: 1,
+              ReaderState.SENSOR_DATA_ACK:1,
+              ReaderState.SAMPLE_DATA: 1,
+              ReaderState.SENSOR_DATA:1,
+              ReaderState.SLEEP_SAMPLE:1,
+              ReaderState.SLEEP_QUERY: 6,
+              ReaderState.SLEEP_SENSOR_DATA: 12}
 
     rf = {'TX_POWER': 0.0}
 
@@ -58,56 +73,123 @@ class Reader(object):
         self.ID = ID
         self.loc = loc
         self.state = state
-        
-        self.last_command_15min = 0
-        self.last_command_1hour = 0
-        self.last_command_12hour = 0
 
     ''' Reader state transition
     '''
-    def next_state(self):
-        current_time = time.time()
-        if self.state == ReaderState.SLEEP:
-            if current_time - self.last_command_15min >= 15 * 60:
-                self.state == ReaderState.SAMPLE_DATA
-                self.last_command_15min = current_time
-                return ReaderParam.timing[ReaderState.SAMPLE_DATA]
-            if current_time - self.last_command_1hour>= 60*60:
-                self.state == ReaderState.QUERY
-                self.last_command_1hour = current_time
-                return ReaderParam.timing[ReaderState.QUERY]
-            if current_time - self.last_command_12hour>=12*60*60:
-                self.state == ReaderState.SELECT
-                self.last_command_12hour = current_time
-                return ReaderParam.timing[ReaderState.SELECT]
+    def next_state(self,counter,tag_list,temp2):
+        
+        if self.state == ReaderState.SLEEP_SAMPLE:
+            print(self.state)
+            
+            self.state = ReaderState.SAMPLE_DATA
+            return ReaderParam.timing[ReaderState.SLEEP_SAMPLE]
         elif self.state == ReaderState.SAMPLE_DATA:
-            self.state = ReaderState.SLEEP
-            return ReaderParam.timing[ReaderState.SLEEP]
+            print(self.state)
+            self.state = ReaderState.SLEEP_SAMPLE
+            print(counter)
+            if counter%4==0 and counter>0:
+                self.state = ReaderState.SELECT_QUERY
+            
+            return T1 + ReaderParam.timing[ReaderState.SLEEP_SAMPLE]
+        elif self.state == ReaderState.SAMPLE_DATA_RX:
+            print(self.state)
+            self.state = ReaderState.SLEEP_SAMPLE
+            return ReaderParam.timing[ReaderState.SLEEP_SAMPLE]
+        elif self.state == ReaderState.SELECT_QUERY:
+             print(self.state)
+            
+             self.state = ReaderState.SELECT_RX_QUERY
+             return ReaderParam.timing[ReaderState.SELECT]
+        elif self.state == ReaderState.SELECT_RX_QUERY:
+            print(self.state)
+            self.state = ReaderState.QUERY
+            return ReaderParam.timing[ReaderState.SELECT]
         elif self.state == ReaderState.QUERY:
+            print(self.state)
             self.state = ReaderState.QUERY_RX
-            return T1 + ReaderParam.timing[ReaderState.RN16] + T2
+            return T1 + ReaderParam.timing[ReaderState.QUERY_ACK] + T2
         elif self.state == ReaderState.QUERY_RX:
-            self.state = ReaderState.ACK
-            return T1 + ReaderParam.timing[ReaderState.RN16] + T2
-        elif self.state == ReaderState.ACK:
-            self.state = ReaderState.ACK_RX
-            return T1 + T3
-        elif self.state == ReaderState.ACK_RX:
-            self.state = ReaderState.Req_RN16
-            return ReaderParam.timing[ReaderState.Req_RN16]
-        elif self.state == ReaderState.Req_RN16:
-            self.state = ReaderState.Tag_ID
-            return T1 + ReaderParam.timing[ReaderState.RN16] + T2
-        
-        
-        if self.state == ReaderState.SELECT:
+            print(self.state)
+            self.state = ReaderState.QUERY_ACK
+            return T1 + ReaderParam.timing[ReaderState.QUERY_ACK] + T2
+        elif self.state == ReaderState.QUERY_ACK:
+            print(self.state)
+            self.state = ReaderState.QUERY_ACK_RX
+            return 1#T1 + T3
+        elif self.state == ReaderState.QUERY_ACK_RX:
+            print(self.state)
+            self.state = ReaderState.QUERY_REQ_RN16
+            return 1#ReaderParam.timing[ReaderState.QUERY_REQ_RN16]
+        elif self.state == ReaderState.QUERY_REQ_RN16:
+            print(self.state)
+                
+            # if counter%48 == 0:
+            #         self.state = ReaderState.SELECT            
+            
+            self.state = ReaderState.TAG_ID
+            return 1#T1 + ReaderParam.timing[ReaderState.QUERY_RN16] + T2
+        elif self.state == ReaderState.TAG_ID:
+            print(self.state)
+            self.state = ReaderState.TAG_ID_RX
+            return 1
+        elif self.state == ReaderState.TAG_ID_RX:
+            print(self.state)
+            print("len of tag_list",len(tag_list))
+            if len(temp2)!=0:
+                self.state = ReaderState.SELECT_QUERY
+                temp2.pop()
+            if len(temp2)==0:
+                for cur_tag in tag_list:
+                    temp2.append(cur_tag)
+                if counter%48 !=0:
+                    self.state = ReaderState.SLEEP_SAMPLE
+                elif counter%48 == 0 and counter>0:
+                    self.state = ReaderState.SELECT 
+            # if counter%48 == 0:
+            #     self.state = ReaderState.SELECT
+            return ReaderParam.timing[ReaderState.SLEEP_SAMPLE]
+        elif self.state == ReaderState.SLEEP_QUERY:
+            print(self.state)
+            
+            self.state = ReaderState.SELECT_QUERY
+            return 1#ReaderParam.timing[ReaderState.QUERY]
+
+        elif self.state == ReaderState.SELECT:
+            print(self.state)
             self.state = ReaderState.SELECT_RX
-            return T4
+            return ReaderParam.timing[ReaderState.SELECT]
 
         elif self.state == ReaderState.SELECT_RX:
-            self.state = ReaderState.QUERY
-            return ReaderParam.timing[ReaderState.QUERY]
-
+            print(self.state)
+            self.state = ReaderState.SENSOR_DATA
+            return ReaderParam.timing[ReaderState.SENSOR_DATA]
+        elif self.state == ReaderState.SENSOR_DATA:
+            self.state = ReaderState.SENSOR_DATA_RX
+            return 1#T1 + ReaderParam.timing[ReaderState.SENSOR_DATA_RN16] + T2
+        elif self.state == ReaderState.SENSOR_DATA_RX:
+            self.state = ReaderState.SENSOR_DATA_ACK
+            return 1#T1 + ReaderParam.timing[ReaderState.SENSOR_DATA_RN16] + T2
+        elif self.state == ReaderState.SENSOR_DATA_ACK:
+            self.state = ReaderState.SENSOR_DATA_ACK_RX
+            return 1#T1 + T3
+        elif self.state == ReaderState.SENSOR_DATA_ACK_RX:
+            self.state = ReaderState.SENSOR_DATA_REQ_RN16
+            return 1#ReaderParam.timing[ReaderState.SENSOR_DATA_REQ_RN16]
+        elif self.state == ReaderState.SENSOR_DATA_REQ_RN16:
+            self.state = ReaderState.DATA
+            return 1#T1 + ReaderParam.timing[ReaderState.SENSOR_DATA_RN16] + T2
+        elif self.state == ReaderState.DATA:
+            if len(temp2)!=0:
+                self.state = ReaderState.SELECT
+                temp2.pop()
+            if len(temp2)==0:
+                for cur_tag in tag_list:
+                    temp2.append(cur_tag)
+                self.state = ReaderState.SLEEP_SAMPLE          
+            return ReaderParam.timing[ReaderState.SLEEP_SAMPLE]
+        elif self.state == ReaderState.SLEEP_SENSOR_DATA:
+            self.state = ReaderState.SLEEP_SAMPLE
+            return ReaderParam.timing[ReaderState.SELECT]
         else:
             return 0.0
 
